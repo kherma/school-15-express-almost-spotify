d4a29f7fe3a5086a0506af512ae9aa33ce7c73c3
kherma <kamil.herma01@gmail.com>
2021-03-21 17:25:30 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+// ================
+// Exports
+// ================
+
 const express = require("express");
 const favicon = require("serve-favicon");
+const hbs = require("express-handlebars");
 const path = require("path");
-const app = express();
+
 const port = 8000;
+const app = express();
+
+// ================
+// Middleware
+// ================
 
 // Static public folder
 app.use(express.static(path.join(__dirname, "/public")));
@@ -25,24 +35,47 @@ app.use((req, res, next) => {
   next();
 });
 
-// Specific paths
+// ================
+// Handlebars
+// ================
+
+// Handlebars Setup
+app.engine(".hbs", hbs());
+app.set("view engine", ".hbs");
+
+// Handlebars paths
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { layout: false, name: req.params.name });
+});
+
+// ================
+// Static paths
+// ================
+
 app.get("/", (req, res) => {
   res.show("index");
 });
 
-app.get("/about", (req, res) => {
-  res.show("about");
+app.get("/playlists", (req, res) => {
+  res.show("playlists");
 });
 
-app.get("/contact", (req, res) => {
-  res.show("contact");
+app.get("/artists", (req, res) => {
+  res.show("artists");
+});
+
+app.get("/albums", (req, res) => {
+  res.show("ablums");
 });
 
 app.use((req, res) => {
   res.status(404).show("404");
 });
 
-// Listen to server
+// ================
+// Start server
+// ================
+
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
